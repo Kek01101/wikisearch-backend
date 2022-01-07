8acb61ae0a869241ab045f0a7c1f66abcc3d2227
Nick Keck <67284308+Kek01101@users.noreply.github.com>
2022-01-07 17:07:39 -0600

    Added wikipedia API hookup

---
+++ NLP_functions.py
@@ -0,0 +1,102 @@
+import nltk, string
+from copy import deepcopy
+from math import log
+from collections import Counter
+
+
+def tokenize(document):
+    """
+    Given a document(or sub-doc like a sentence), this function will return a list of all the lowercase words in that
+    document.
+
+    This also removes any punctuation or "stopwords" - common meaningless words such as "an", "and", etc.
+
+    Results of tokenization immediately saved to sentence dict, where each sentence is represented by its sentence
+    tokens.
+    """
+    # word_tokenize splits each word and punctuation into a separate item in a list - all lowercase
+    words = nltk.word_tokenize(document.lower())
+    # remove punctuation and stopwords from words list
+    for word in deepcopy(words):
+        if word in string.punctuation or word in nltk.corpus.stopwords.words('english'):
+            words.remove(word)
+    return words
+
+
+def calc_idfs(documents):
+    """
+    Given a dictionary of a document or many sentences mapped from sentence/doc: words, return dictionary mapping words
+    to IDF values.
+
+    IDF - Inverse document frequency, how rare a word is within a "document"
+
+    Results from the comp_idfs will be used for more document recommendations as well as calculating query matches
+    """
+    words = dict()
+    clearlist = []
+    for doc in documents:
+        for word in documents[doc]:
+            if word not in clearlist:
+                clearlist.append(word)
+    for word in clearlist:
+        count = 0
+        for doc in documents:
+            if word in documents[doc]:
+                count += 1
+        words[words] = log((len(documents))/count)
+    return words
+
+
+def sentence_match(query, sentences, word_scores, n=3):
+    """
+    Given a query, sentences, and the IDF values of the words in those sentences, provide the n best sentence
+    matches for said query. If there is an IDF tie, the query term density will be used to settle the tie.
+    """
+    def sortByScore(sList):
+        return sList[1]
+    rankings = []
+    output = []
+    for sentence in sentences:
+        score = 0
+        qtd = 0
+        for word in query:
+            for term in sentences[sentence]:
+                if word == term:
+                    score += word_scores[word]
+                    qtd += 1
+        qtd = qtd/len(sentences[sentence])
+        rankings.append(((sentence, qtd), score))
+    rankings.sort(key=sortByScore, reverse=True)
+    for a in range(n):
+        output.append(rankings[a][0])
+    # Fix this, this does not take into account IDF for best match
+    output.sort(key=sortByScore, reverse=True)
+    for a in range(n):
+        output[n] = output[n][0]
+    return output
+
+
+def article_match(query, articles, article_scores, n=2):
+    """
+    Finds other articles already within the DB which may also be helpful for the user. Functions in much the same way
+    that the sentence_match function does.
+
+    Full TF-IDF is used here, whereas only IDF is used for the actual sentence matching. This is because this considers
+    many documents, and sentence match only considers one at a time.
+    """
+    def sortByScore(sList):
+        return sList[1]
+    rankings = []
+    output = []
+    for article in articles:
+        articles[article] = Counter(articles[article]).most_common()
+        score = 0
+        for word in query:
+            for term, frequency in articles[article]:
+                if word == term:
+                    score += float(frequency)*article_scores[term]
+        rankings.append((article, score))
+    rankings.sort(key=sortByScore, reverse=True)
+    for a in range(n):
+        output.append(rankings[a][0])
+    return output
--- app.py
+++ app.py
@@ -1,15 +1,26 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
-import psycopg2, json
+import psycopg2, json, requests, wikipediaapi, nltk
+from NLP_functions import tokenize, calc_idfs, sentence_match
 
-# Obligatory app setup
-id_count = 2
+# Flask app setup
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
+# Database connection setup
 conn = psycopg2.connect(host="ec2-54-163-254-204.compute-1.amazonaws.com", dbname="dpt2unfirlin7", user="gtktnnqqdhhtga",
                         password="eba958fc92598e3d580d84917af58f67488ea2c64d4af83ebfc096bd082bc532")
 cur = conn.cursor()
+# Database primary key variable setup
+id_count = 0
+# Wikipedia API link setup
+placeholder = "[PLACEHOLDER]"
+api_search = f"http://en.wikipedia.org/w/api.php?action=query&list=search&srsearch={placeholder}&srlimit=5&format=json"
+# Wikipedia API extraction setup
+wiki_wiki = wikipediaapi.Wikipedia(
+    language='en',
+    extract_format=wikipediaapi.ExtractFormat.WIKI
+)
 
 
 @app.route('/')
@@ -41,10 +52,73 @@ def dbcheck():
     id_count += 1
     test_json_1 = {"test": True, "type": 1}
     test_json_2 = {"test": "True", "type": 2}
-    score = int(request.args.get("score", None))
+    score = str(request.args.get("score", None))
+    # DB values are (PK, article text, article tokens, article idfs)
     cur.execute("INSERT INTO main VALUES (%s, %s, %s, %s)", (id_count, score, json.dumps(test_json_1), json.dumps(test_json_2)))
     conn.commit()
     return jsonify({"msg": "Data saved to SQL database successfully"})
+
+
+@app.route('/wikisearch/', methods=['GET'])
+@cross_origin()
+def wiki_search():
+    # Handles the actual wiki searching, handles all functionality using appropriate modules
+    query = str(request.args.get("query", None))
+    subject = str(request.args.get("subject", None))
+
+    # Formats and sends a request to the wikipedia API for 5 most-related pages to subject
+    query.replace(' ', "%20")
+    page_search = api_search.replace(placeholder, query)
+    # Saves top 5 returned pages to top_pages list
+    top_pages = requests.get(page_search).json()["query"]["search"]
+
+    """
+    Wiki page sanitization
+    """
+    # Would normally ask user for preferred page, just pulls from the top page for now
+    p_wiki = wiki_wiki.page(top_pages["title"][0])
+    article = str(p_wiki.text)
+
+    """
+    NLP
+    """
+    # saving article tokens and idfs to an array for DB saving
+    article_info = [tokenize(article), calc_idfs(article)]
+    # split document into a list of ordered tokens and save to sentence dict
+    sentences = dict()
+    for sentence in nltk.sent_tokenize(article):
+        tokens = tokenize(sentence)
+        if tokens:
+            sentences[sentence] = tokens
+    # calculate IDF values for each sentence and save to word_score dict
+    word_score = calc_idfs(sentences)
+
+    """
+    DB Hookup
+    """
+    global id_count
+    id_count += 1
+    # Saving the new ripped article values to the DB - add function for updating old articles later
+    cur.execute("INSERT INTO main VALUES (%s, %s, %s, %s)",
+                (id_count, article, article_info[0], article_info[1]))
+    conn.commit()
+
+    """
+    Query matching
+    """
+    # Saving 3 most relevant sentences to the query to "top_sentences" array
+    top_sentences = sentence_match(query, sentences, word_score)
+
+    """
+    Placeholder response - for now
+    """
+    res = {
+        "article": article,
+        "sentence_1": top_sentences[0],
+        "sentence_2": top_sentences[1],
+        "sentence_3": top_sentences[2]
+    }
+    return jsonify(res)
 
 
 if __name__ == '__main__':
