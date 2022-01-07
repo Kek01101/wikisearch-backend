182c3cf5c5ec3e53aa529c44e2fb4f99cb53b216
Nick Keck <67284308+Kek01101@users.noreply.github.com>
2022-01-07 15:21:34 -0600

    Created a DB test to see if values can be passed to database

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
+import psycopg2
 
+# Obligatory app setup
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
+conn = psycopg2.connect("host=ec2-54-163-254-204.compute-1.amazonaws.com dbname=dpt2unfirlin7 user=gtktnnqqdhhtga")
+cur = conn.cursor()
 
 
 @app.route('/')
@@ -24,6 +28,19 @@ def apicheck():
     # Checking that sending responses works too
     res = {"msg": f"Msg: {msg2}"}
     return jsonify(res)
+
+
+@app.route('/db-test/', methods=['GET'])
+@cross_origin()
+def dbcheck():
+    # Checking that intended message is received, and that DB saving works
+    test_json_1 = {"test": True, "type": 1}
+    test_json_2 = {"test": "True", "type": 2}
+    score = int(request.args.get("score", None))
+    print(score)
+    cur.execute("INSERT INTO MAIN VALUES (%s, %s, %s)", (score, jsonify(test_json_1), jsonify(test_json_2)))
+    conn.commit()
+    return jsonify({"msg": "Data saved to SQL database successfully"})
 
 
 if __name__ == '__main__':
