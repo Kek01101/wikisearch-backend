fdfa094a6b26182ad6f396837ba225169838c080
Nick Keck <67284308+Kek01101@users.noreply.github.com>
2022-01-08 07:42:34 -0600

    re-deploy

--- NLP_functions.py
+++ NLP_functions.py
@@ -68,6 +68,7 @@ def sentence_match(query, sentences, word_scores, n=3):
         rankings.append((sentence, score, qtd))
     rankings.sort(key=sortByScore, reverse=True)
     print(rankings)
+
     raise NotImplementedError
     for a in range(n):
         output.append((rankings[0], rankings[1]))
